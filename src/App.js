0d0671650877563d0543c6b85de113be14a888fe
Jun <git config --global user.email junbob1218@gmail.com>
2024-01-25 17:16:19 +0900

    첫 번쨰 커밋

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+//axios 이용해서 네이버 검색 api (뉴스)데이터 가져와보기!!
+import axios from 'axios'
+import {useEffect} from 'react'
+
+function App() {
+//http://localhost:3000
+
+//env 변수 가져오기
+const client_id = process.env.REACT_APP_CLIENT_ID
+const secret = process.env.REACT_APP_SECRETE
+
+  useEffect(function(){
+    axios({
+      url:"/v1/search/news.json",
+      params:{query : "짜장"},
+      headers :{
+        'X-Naver-Client-Id' : client_id,
+        'X-Naver-Client-Secret': secret
+      }
+    })
+    .then((result)=>{
+      console.log(result)
+    })
+  })
+
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
